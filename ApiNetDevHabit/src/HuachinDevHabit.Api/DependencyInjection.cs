63aa62bd61715b71b34d9cceba80adc7e9378e18
Francisco <jhersonhuacho@gmail.com>
2025-05-12 16:23:11 -0500

    03:Hateos: Fix Media Types

--- ApiNetDevHabit/src/HuachinDevHabit.Api/DependencyInjection.cs
+++ ApiNetDevHabit/src/HuachinDevHabit.Api/DependencyInjection.cs
@@ -19,150 +19,149 @@ using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
-namespace HuachinDevHabit.Api
+namespace HuachinDevHabit.Api;
+
+public static class DependencyInjection
 {
-	public static class DependencyInjection
+	public static WebApplicationBuilder AddApiServices(this WebApplicationBuilder builder)
 	{
-		public static WebApplicationBuilder AddApiServices(this WebApplicationBuilder builder)
-		{
-			#region Configuración de Controladores
-			builder.Services.AddControllers(options =>
-				{
-					options.ReturnHttpNotAcceptable = true;
-				})
-				.AddNewtonsoftJson(options =>
-				{
-					options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-				})
-				.AddXmlSerializerFormatters();
-			#endregion
-
-			#region Configuración de Serializadores: Agregar el nuevo Media Type para HATEOS
-			builder.Services.Configure<MvcOptions>(options =>
+		#region Configuración de Controladores
+		builder.Services.AddControllers(options =>
 			{
-				NewtonsoftJsonOutputFormatter formatter = options.OutputFormatters
-					.OfType<NewtonsoftJsonOutputFormatter>()
-					.First();
-
-				// Esto agrega Media Type application/vnd.dev-habit.hateos+json de forma global
-				formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.JsonV1);
-				formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.JsonV2);
-				formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.HateosJson);
-				formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.HateosJsonV1);
-				formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.HateosJsonV2);
-			});
-			#endregion
-
-			#region Configuración de Versionado de API
-			builder.Services
-				.AddApiVersioning(options =>
-				{
-					options.DefaultApiVersion = new Asp.Versioning.ApiVersion(1.0);
-					options.AssumeDefaultVersionWhenUnspecified = true;
-					options.ReportApiVersions = true;
-					//options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
-					options.ApiVersionSelector = new DefaultApiVersionSelector(options);
-
-					//options.ApiVersionReader = new UrlSegmentApiVersionReader();
-					options.ApiVersionReader = ApiVersionReader.Combine(
-						new MediaTypeApiVersionReader("application/vnd.dev-habit.hateos.{version}+json"),
-						new MediaTypeApiVersionReaderBuilder()
-							.Template("")
-							.Build());
-				})
-				.AddMvc();
-			#endregion
-
-			return builder;
-		}
-
-		public static WebApplicationBuilder AddSwagger(this WebApplicationBuilder builder)
-		{
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
-
-			return builder;
-		}
-
-		public static WebApplicationBuilder AddErrorHandling(this WebApplicationBuilder builder)
-		{
-			builder.Services.AddProblemDetails(options =>
+				options.ReturnHttpNotAcceptable = true;
+			})
+			.AddNewtonsoftJson(options =>
 			{
-				options.CustomizeProblemDetails = context =>
-				{
-					context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
-				};
-			});
+				options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+			})
+			.AddXmlSerializerFormatters();
+		#endregion
 
-			builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
-			builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-			return builder;
-		}
-
-		public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
+		#region Configuración de Serializadores: Agregar el nuevo Media Type para HATEOS
+		builder.Services.Configure<MvcOptions>(options =>
 		{
-			builder.Services.AddDbContext<ApplicationDbContext>(options =>
+			NewtonsoftJsonOutputFormatter formatter = options.OutputFormatters
+				.OfType<NewtonsoftJsonOutputFormatter>()
+				.First();
+
+			// Esto agrega Media Type application/vnd.dev-habit.hateos+json de forma global
+			formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.JsonV1);
+			formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.JsonV2);
+			formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.HateosJson);
+			formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.HateosJsonV1);
+			formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.HateosJsonV2);
+		});
+		#endregion
+
+		#region Configuración de Versionado de API
+		builder.Services
+			.AddApiVersioning(options =>
 			{
-				options
-					.UseNpgsql(
-						builder.Configuration.GetConnectionString("Database"),
-						npgsqlOptions => npgsqlOptions
-							.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
-					.UseSnakeCaseNamingConvention();
-			});
+				options.DefaultApiVersion = new Asp.Versioning.ApiVersion(1.0);
+				options.AssumeDefaultVersionWhenUnspecified = true;
+				options.ReportApiVersions = true;
+				//options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
+				options.ApiVersionSelector = new DefaultApiVersionSelector(options);
 
-			return builder;
-		}
+				//options.ApiVersionReader = new UrlSegmentApiVersionReader();
+				options.ApiVersionReader = ApiVersionReader.Combine(
+					new MediaTypeApiVersionReader(),
+					new MediaTypeApiVersionReaderBuilder()
+						.Template("application/vnd.dev-habit.hateos.{version}+json")
+						.Build());
+			})
+			.AddMvc();
+		#endregion
 
-		public static WebApplicationBuilder AddObservability(this WebApplicationBuilder builder)
+		return builder;
+	}
+
+	public static WebApplicationBuilder AddSwagger(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+
+		return builder;
+	}
+
+	public static WebApplicationBuilder AddErrorHandling(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddProblemDetails(options =>
 		{
-			// Configuration OpenTelemetry
-			builder.Services.AddOpenTelemetry()
-				.ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
-				.WithTracing(tracing => tracing
-					.AddHttpClientInstrumentation()
-					.AddAspNetCoreInstrumentation()
-					.AddNpgsql())
-				.WithMetrics(metrics => metrics
-					.AddHttpClientInstrumentation()
-					.AddAspNetCoreInstrumentation()
-					.AddRuntimeInstrumentation())
-				.UseOtlpExporter();
-
-			builder.Logging.AddOpenTelemetry(options =>
+			options.CustomizeProblemDetails = context =>
 			{
-				options.IncludeScopes = true;
-				options.IncludeFormattedMessage = true;
-			});
+				context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+			};
+		});
 
-			return builder;
-		}
+		builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
+		builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
-		public static WebApplicationBuilder AddApplicationServices(this WebApplicationBuilder builder)
+		return builder;
+	}
+
+	public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddDbContext<ApplicationDbContext>(options =>
 		{
-			#region FluentValidation
-			//builder.Services.AddValidatorsFromAssemblyContaining<Program>(includeInternalTypes: true);
-			builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-			#endregion
+			options
+				.UseNpgsql(
+					builder.Configuration.GetConnectionString("Database"),
+					npgsqlOptions => npgsqlOptions
+						.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+				.UseSnakeCaseNamingConvention();
+		});
 
-			#region Sorting and Data Shaping
-			// Sorting
-			builder.Services.AddTransient<SortMappingProvider>();
-			builder.Services.AddSingleton<ISortMappingDefinition, SortMappingDefinition<HabitDto, Habit>>(_ =>
-				HabitMapping.SortMapping);
-			// Data Shaping
-			builder.Services.AddTransient<DataShapingService>();
-			#endregion
+		return builder;
+	}
 
-			// Registramos AddHttpContextAccessor para poder usar IHttpContextAccessor en el servicio de HATEOS
-			builder.Services.AddHttpContextAccessor();
+	public static WebApplicationBuilder AddObservability(this WebApplicationBuilder builder)
+	{
+		// Configuration OpenTelemetry
+		builder.Services.AddOpenTelemetry()
+			.ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+			.WithTracing(tracing => tracing
+				.AddHttpClientInstrumentation()
+				.AddAspNetCoreInstrumentation()
+				.AddNpgsql())
+			.WithMetrics(metrics => metrics
+				.AddHttpClientInstrumentation()
+				.AddAspNetCoreInstrumentation()
+				.AddRuntimeInstrumentation())
+			.UseOtlpExporter();
 
-			#region HATEOS
-			builder.Services.AddTransient<LinkService>();
-			#endregion
+		builder.Logging.AddOpenTelemetry(options =>
+		{
+			options.IncludeScopes = true;
+			options.IncludeFormattedMessage = true;
+		});
 
-			return builder;
-		}
+		return builder;
+	}
+
+	public static WebApplicationBuilder AddApplicationServices(this WebApplicationBuilder builder)
+	{
+		#region FluentValidation
+		//builder.Services.AddValidatorsFromAssemblyContaining<Program>(includeInternalTypes: true);
+		builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+		#endregion
+
+		#region Sorting and Data Shaping
+		// Sorting
+		builder.Services.AddTransient<SortMappingProvider>();
+		builder.Services.AddSingleton<ISortMappingDefinition, SortMappingDefinition<HabitDto, Habit>>(_ =>
+			HabitMapping.SortMapping);
+		// Data Shaping
+		builder.Services.AddTransient<DataShapingService>();
+		#endregion
+
+		// Registramos AddHttpContextAccessor para poder usar IHttpContextAccessor en el servicio de HATEOS
+		builder.Services.AddHttpContextAccessor();
+
+		#region HATEOS
+		builder.Services.AddTransient<LinkService>();
+		#endregion
+
+		return builder;
 	}
 }
--- ApiNetDevHabit/src/HuachinDevHabit.Api/Services/ContentNegotiation/CustomMediaTypeNames.cs
+++ ApiNetDevHabit/src/HuachinDevHabit.Api/Services/ContentNegotiation/CustomMediaTypeNames.cs
@@ -6,9 +6,9 @@
 		{
 			public const string JsonV1 = "application/json;v=1";
 			public const string JsonV2 = "application/json;v=2";
-			public const string HateosJson = "application/vnd.dev-habit.hateos+json";
-			public const string HateosJsonV1 = "application/vnd.dev-habit.hateos.1+json";
-			public const string HateosJsonV2 = "application/vnd.dev-habit.hateos.2+json";
+			public const string HateosJson = "application/vnd.dev-habit.hateoas+json";
+			public const string HateosJsonV1 = "application/vnd.dev-habit.hateoas.1+json";
+			public const string HateosJsonV2 = "application/vnd.dev-habit.hateoas.2+json";
 		}
 	}
 }
